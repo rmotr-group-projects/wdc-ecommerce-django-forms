6505734e328a0f84c8ebcaf45ec354ede336381a
Ivan Zugnoni <zugnoni.ivan@gmail.com>
2018-12-03 16:23:58 -0300

    Fix template settings

--- ecommerce_django_forms/ecommerce_django_forms/settings/base.py
+++ ecommerce_django_forms/ecommerce_django_forms/settings/base.py
@@ -14,6 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DJANGO_PROJECT_DIR = os.path.dirname(BASE_DIR)
+PROJECT_DIR = os.path.dirname(DJANGO_PROJECT_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -62,7 +64,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates')
+            os.path.join(BASE_DIR, os.path.pardir, 'templates')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
