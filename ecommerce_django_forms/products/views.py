2162142581d9a7563e5cfd288dcbf7190ce11de0
Jonathan Flowers <jmflowers134@gmail.com>
2019-03-25 18:02:34 -0400

    Update views.py

--- ecommerce_django_forms/products/views.py
+++ ecommerce_django_forms/products/views.py
@@ -6,11 +6,14 @@ from products.models import Product, Category, ProductImage
 
 
 def products(request):
-    # Get all products from the DB
-    products = '...'
+    #Get all products from the DB
+    products = Product.objects.all()
 
     # Get up to 4 featured products from the DB
-    featured_products = '...'
+    if products.filter(featured=True).count()>=4:
+        featured_products = products.filter(featured=True)[:4]
+    else:
+        featured_products = products.filter(featured=True)
 
     # This is done for you. The request.session is a dictionary that contains
     # data that will be stored as long as the user is authenticated.
@@ -20,129 +23,163 @@ def products(request):
     request.session.save()
 
     # Render 'products.html' template sending products and featured products
-    # as context
-    return '...'
+    #as context
+    return render(request,'products.html',
+        context = {
+            'products':products,
+            'featured_products':featured_products,
+        }
+        )
 
 
 def create_product(request):
     if request.method == 'GET':
         # Create an empty instance of ProductFrom() and render "create_product.html"
         # sending the form as context under "product_form" key
-        product_form = '...'
-        return '...'
+        product_form = ProductForm()
+        return render(request,'create_product.html',
+        context = {'product_form':product_form}
+        )
+        
     elif request.method == 'POST':
-        # Create an instance of ProductFrom initializing it with the product
+        #Create an instance of ProductFrom initializing it with the product
         # data that come in request.POST
-        product_form = '...'
+        product_form = ProductForm(request.POST)
+        
         if product_form.is_valid():
-
-            # Create the product object while saving the form
-            product = '...'
-
+            #Create the product object while saving the form
+            product = product_form.save()
+            
             # Inside product_form.cleaned_data you will find the already
             # validated data for the product. Use the images urls there to
             # create a ProductImage object for each one.
-
-            # YOUR CODE HERE
+            images = []
+            for i in range(3):
+                pic = product_form.cleaned_data['image_{}'.format(i+1)]
+                if pic:
+                    images.append([pic])
+            for pic in images:
+                ProductImage.objects.create(product = product,url=pic)
 
             # Redirect to products view
-            return '...'
+            return redirect('products')
 
         # If form is not valid, re-render the 'create_product.html' sending the
         # product_form as context, which will have all the error messages included
-        return '...'
+        return render(request,'create_product.html',context = {'product_form':product_form})
 
 
 def edit_product(request, product_id):
     # Get the product with given product_id from the DB
-    product = '...'
+    product = Product.objects.get(id=product_id)
+    
     if request.method == 'GET':
         # Create an instance of ProductForm sending the product in the "instance"
-        # parameter. This will initialize all the fields in the form with the
+        #parameter. This will initialize all the fields in the form with the
         # data from our product.
-        product_form = '...'
+        product_form = ProductForm(instance=product)
 
         # Render the 'edit_product.html' template sending the product and the
         # product_form as context
-        return '...'
+        return render(request,'edit_product.html',
+        context = {
+            'product':product,
+            'product_form':product_form}
+        )
     elif request.method == 'POST':
-        # Create an instance of ProductForm sending the new data that come in
+        #Create an instance of ProductForm sending the new data that come in
         # request.POST, and also the product inside the "instance" parameter
-        product_form = '...'
+        product_form = ProductForm(request.POST,instance=product)
         if product_form.is_valid():
-            # Create the product object while saving the form
-            product = '...'
+            #Create the product object while saving the form
+            product = product_form.save()
 
             # Inside product_form.cleaned_data you will find the already
             # validated data for the product. Use the images urls there to
             # create a ProductImage object that are not already created in the DB.
             # The ones that didn't come in cleaned_data and are stored in the DB
             # should be deleted.
-
-            # YOUR CODE HERE
-
+            current_images = product.productimage_set.all()
+            current_images_urls = [pic.url for pic in current_images]
+            new_images = []
+            for i in range(3):
+                input_url = product_form.cleaned_data['image_{}'.format(i+1)]
+                if input_url:
+                    new_images.append(input_url)
+                    ProductImage.objects.create(product = product,url=input_url)
+            for url in current_images_urls:
+                if url not in new_images:
+                    del_pic = product.productimage_set.filter(url=url)
+                    del_pic.delete()
             # Redirect to 'products' view
             return redirect('products')
 
         # If form is not valid, re-render the 'create_product.html' sending the
         # product and the product_form as context
-        return '...'
+        return render(request,'edit_product.html',
+        context = {
+            'product':product,
+            'product_form':product_form}
+        )
 
 
 def delete_product(request, product_id):
-    # Get the product with given product_id from the DB
-    product = '...'
+    #Get the product with given product_id from the DB
+    product = Product.objects.get(id=product_id)
     if request.method == 'GET':
         # Render the 'delete_product.html' template sending the product as context
-        return '...'
+        return render(request,'delete_product.html',
+        context = {'product':product}
+        )
     elif request.method == "POST":
-        # Delete the product and redirect to 'products' view
-        # YOUR CODE HERE
-        return '...'
+        #Delete the product and redirect to 'products' view
+        product.delete()
+        return redirect('products')
 
 
 def toggle_featured(request, product_id):
-    # Get the product with given product_id from the DB
-    product = '...'
+    #Get the product with given product_id from the DB
+    product = Product.objects.get(id=product_id)
 
-    # Toggle the boolean product.featured and save the product
-    # YOUR CODE HERE
-
+    #Toggle the boolean product.featured and save the product
+    product.featured = not product.featured
+    product.save()
     # Redirect to 'products' view
-    return '...'
+    return redirect('products')
 
 
 def cart(request):
-    # Get all the products ids from the products that have been added to the cart.
+    #Get all the products ids from the products that have been added to the cart.
     # You can find this ids in the request.session dictionary, under the
     # 'products_in_cart' key. This was explained and initialized in the 'products' view
-    products_ids = '...'
+    products_ids = request.session['products_in_cart']
 
-    # Get all the products from the DB with the ids above
-    products_in_cart = '...'
-
+    #Get all the products from the DB with the ids above
+    products_in_cart = Product.objects.filter(id__in=products_ids)
     # Render the 'cart.html' template sending the products_in_cart as context
-    return '...'
+    return render(request,'cart.html',context={'products_in_cart':products_in_cart})
 
 
 def add_to_cart(request):
-    # Get the product_id that come in request.POST and add it to the
-    # list under 'products_in_cart' key of request.session dictionary.
+    #Get the product_id that come in request.POST and add it to the
+    #list under 'products_in_cart' key of request.session dictionary.
     # Do a .save() to the request.session. This is the way that Django recognizes
-    # that something changes in the session.
-
-    # YOUR CODE HERE
+    #that something changes in the session.
+    product_id = request.POST['product_id']
+    request.session['products_in_cart'].append(product_id)
+    request.session.save()
 
     # Redirect to 'products' view
-    return '...'
+    return redirect('products')
 
 
 def remove_from_cart(request):
-    # Get the product_id that come in request.POST and remove it from the list
+    #Get the product_id that come in request.POST and remove it from the list
     # under 'products_in_cart' of request.session dictionary.
     # Do a .save() to the request.session.
 
-    # YOUR CODE HERE
-
+    product_id = request.POST['product_id']
+    request.session['products_in_cart'].remove(product_id)
+    request.session.save()
     # Redirect to 'cart' view
-    return '...'
+    return redirect('cart')
