daae91c87bb66aee9f20b5b6b3475268887646c3
daniellefuller00 <daniellefuller00@gmail.com>
2019-06-17 00:05:37 +0000

    added solutions

--- ecommerce_django_forms/products/forms.py
+++ ecommerce_django_forms/products/forms.py
@@ -24,14 +24,17 @@ class ProductForm(forms.ModelForm):
     image_3 = forms.URLField(required=False)
 
     class Meta:
-        # Complete model and fields
-        # YOUR CODE HERE
-        pass
+        model = Product
+        fields = ['name', 'sku', 'category', 'description', 'price']
+        
 
     def __init__(self, *args, **kwargs):
         super(ProductForm, self).__init__(*args, **kwargs)
         product = self.instance
-        # Loop through all product images and assign one image URL to each
-        # image_1, image_2 and image_3 form fields
+        product_images = [image.url for image in product.productimage_set.all()]
 
-        # YOUR CODE HERE
+        for i in range(len(product_images)):
+            field = self.fields.get('image_{}'.format(i + 1))
+            field.initial = product_images[i]
+        
+            
